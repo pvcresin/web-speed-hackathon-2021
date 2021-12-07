5b57b172ef35e3ab5a672d72459a2035755ef664
pvcresin <pvcresin0730@gmail.com>
2021-12-07 23:38:42 +0900

    Keep aspect ratio pausable movie

--- client/src/components/foundation/PausableMovie/PausableMovie.jsx
+++ client/src/components/foundation/PausableMovie/PausableMovie.jsx
@@ -65,25 +65,23 @@ const PausableMovie = ({ src }) => {
     });
   }, []);
 
-  if (isLoading || data === null) {
-    return null;
-  }
-
   return (
     <AspectRatioBox aspectHeight={1} aspectWidth={1}>
-      <button className="group relative block w-full h-full" onClick={handleClick} type="button">
-        <canvas ref={canvasCallbackRef} className="w-full" />
-        <div
-          className={classNames(
-            'absolute left-1/2 top-1/2 flex items-center justify-center w-16 h-16 text-white text-3xl bg-black bg-opacity-50 rounded-full transform -translate-x-1/2 -translate-y-1/2',
-            {
-              'opacity-0 group-hover:opacity-100': isPlaying,
-            },
-          )}
-        >
-          <FontAwesomeIcon iconType={isPlaying ? 'pause' : 'play'} styleType="solid" />
-        </div>
-      </button>
+      {isLoading || data === null ? null : (
+        <button className="group relative block w-full h-full" onClick={handleClick} type="button">
+          <canvas ref={canvasCallbackRef} className="w-full" />
+          <div
+            className={classNames(
+              'absolute left-1/2 top-1/2 flex items-center justify-center w-16 h-16 text-white text-3xl bg-black bg-opacity-50 rounded-full transform -translate-x-1/2 -translate-y-1/2',
+              {
+                'opacity-0 group-hover:opacity-100': isPlaying,
+              },
+            )}
+          >
+            <FontAwesomeIcon iconType={isPlaying ? 'pause' : 'play'} styleType="solid" />
+          </div>
+        </button>
+      )}
     </AspectRatioBox>
   );
 };
