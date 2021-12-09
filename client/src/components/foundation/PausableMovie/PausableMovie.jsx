f820788076bfa0ab83e9fdd10ac2bfc6fc027836
pvcresin <pvcresin0730@gmail.com>
2021-12-10 01:42:24 +0900

    Make video loop

--- client/src/components/foundation/PausableMovie/PausableMovie.jsx
+++ client/src/components/foundation/PausableMovie/PausableMovie.jsx
@@ -35,7 +35,7 @@ const PausableMovie = ({ src }) => {
   return (
     <AspectRatioBox aspectHeight={1} aspectWidth={1}>
       <button className="group relative block w-full h-full" onClick={handleClick} type="button">
-        <video className="w-full" ref={animatorRef} src={src} autoPlay muted />
+        <video className="w-full" ref={animatorRef} src={src} autoPlay muted loop />
         <div
           className={classNames(
             'absolute left-1/2 top-1/2 flex items-center justify-center w-16 h-16 text-white text-3xl bg-black bg-opacity-50 rounded-full transform -translate-x-1/2 -translate-y-1/2',
