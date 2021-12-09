ee9b15ad2c3b52c1495add3ed838ad50c86d8356
pvcresin <pvcresin0730@gmail.com>
2021-12-09 22:56:18 +0900

    Remove webpack mode setting

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -36,7 +36,6 @@ const config = {
       path.resolve(SRC_PATH, './index.jsx'),
     ],
   },
-  mode: 'none',
   module: {
     rules: [
       {
@@ -66,7 +65,7 @@ const config = {
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || '',
-      NODE_ENV: 'development',
+      NODE_ENV: process.env.NODE_ENV || 'production',
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
